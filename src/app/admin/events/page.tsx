0106c6535397aa713884d6f0f57619eef0ff02a2
puwadol-vi <puwadolchain@gmail.com>
2026-03-10 00:24:05 +0700

    fix: admin page

--- src/app/admin/events/page.tsx
+++ src/app/admin/events/page.tsx
@@ -1,6 +1,5 @@
 import { AdminEventsPageClient } from "./admin-events-client";
 
-export const runtime = "edge";
 export const dynamic = "force-dynamic";
 
 export default function AdminEventsPage() {
--- src/app/admin/spots/page.tsx
+++ src/app/admin/spots/page.tsx
@@ -1,6 +1,5 @@
 import { AdminSpotsPageClient } from "./admin-spots-client";
 
-export const runtime = "edge";
 export const dynamic = "force-dynamic";
 
 export default function AdminSpotsPage() {
